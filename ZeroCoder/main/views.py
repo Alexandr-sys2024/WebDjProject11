cc8c017a65de4c204e9746b422a8f99f14579946
Alexandr-sys2024 <time2016slot@gmail.com>
2024-11-30 08:21:35 +0300

    add new code in views in main

--- ZeroCoder/main/views.py
+++ ZeroCoder/main/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+# Главная страница
+def index(request):
+    data = {
+        'caption': "CatDjango"
+    }
+    return render(request, 'main/index.html', data)
+
+# Второстепенная страница
+def new(request):
+    return render(request, 'main/new.html')
+
+# О нас
+def about(request):
+    return render(request, 'main/about.html')
+
+# Контакты
+def contact(request):
+    return render(request, 'main/contact.html')
+from django.shortcuts import render
 
 # Create your views here.
