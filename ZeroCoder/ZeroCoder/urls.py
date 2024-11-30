0598cbec73575b2cd951c34032462ecb35c4b249
Alexandr-sys2024 <time2016slot@gmail.com>
2024-11-30 09:30:05 +0300

    add new file urlspy in news

--- ZeroCoder/ZeroCoder/urls.py
+++ ZeroCoder/ZeroCoder/urls.py
@@ -17,7 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from ZeroCoder.ZeroCoder import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls'))
-]
+    path('', include('main.urls')),
+    path('news/', include('news.urls'))
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
