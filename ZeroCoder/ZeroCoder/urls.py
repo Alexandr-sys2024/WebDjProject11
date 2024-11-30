8ab0f05fb28bbed08e02b5c6b6b36851218548ee
Alexandr-sys2024 <time2016slot@gmail.com>
2024-11-30 09:57:36 +0300

    add new code in viewspy in news

--- ZeroCoder/ZeroCoder/urls.py
+++ ZeroCoder/ZeroCoder/urls.py
@@ -17,10 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from ZeroCoder.ZeroCoder import settings
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('news/', include('news.urls'))
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
